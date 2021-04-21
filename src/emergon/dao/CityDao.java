528fbae4a4a4d8cf6b8526e0de09f6f46559b774
Anastasios Lelakis <tasos@hotmail.com>
2021-04-21 21:33:56 +0300

    add String Queries in CityDao

--- src/emergon/dao/CityDao.java
+++ src/emergon/dao/CityDao.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class CityDao extends GenericDao implements CrudInterface<City>{
 
+    private static final String FINDBYID = "SELECT * FROM city WHERE city_id = ?";
+    private static final String FINDALL = "SELECT * FROM city";
+    private static final String INSERT = "INSERT INTO city (city, country_id, last_update) VALUES (?, ?, ?)";
+    private static final String UPDATE = "UPDATE city SET city = ?, country_id = ?, last_update = ? WHERE city_id = ?";
+    private static final String DELETE = "DELETE FROM city WHERE city_id = ?";
+    //private static final String FINDBYMAXID = "SELECT * FROM city WHERE city_id = (SELECT max(city_id) FROM city)";
+
     @Override
     public List<City> findAll() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
